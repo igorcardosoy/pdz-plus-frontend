82656da06f43fb5aacb2a875b0805e41a89f9a1e
igorcardosoy <igofilipicardoso@gmail.com>
2025-07-30 18:20:12 -0300

    fix: update next.config.ts to include rewrites for API requests

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig: import('next').NextConfig = {
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: 'http://localhost:8080/pdz-api/:path*',
+      },
+    ];
+  },
 };
+
+module.exports = nextConfig;
 
 export default nextConfig;
